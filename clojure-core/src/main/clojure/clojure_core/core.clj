92dbdd1bbd46c3bf3ebaa0570a64fc915dd4c491
Kuldeep Singh <kuldeepsinghk.com>
2018-08-15 00:23:00 +0530

    Clojure module as well

---
+++ clojure-core/src/main/clojure/clojure_core/core.clj
@@ -0,0 +1,8 @@
+(ns clojure-core.core
+  (:gen-class :name com.multi.ClojureMain :methods [#^{:static true} [sum [int int] int]]))
+
+(defn sum [a b] (+ a b))
+
+
+(defn -sum "A Java-callable wrapper around the 'sum' function."  [n k]
+  (sum n k))
---
+++ java-client/src/main/java/com/multi/ClojureClient.java
@@ -0,0 +1,9 @@
+package com.multi;
+
+public class ClojureClient {
+
+    public static void main(String[] args) {
+
+
+    }
+}
