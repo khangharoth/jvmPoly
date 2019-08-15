56eadca467bc15ec354cc0304518afc4ae758eef
Kuldeep Singh <kuldeepsinghk.com>
2019-08-15 10:28:00 +0530

    Quick sort in Clojure

---
+++ clojure-core/src/test/clojure/quick_sort_test.clj
@@ -0,0 +1,7 @@
+(ns quick-sort-test
+  (:require [clojure.test :refer :all]
+            [data-str.quick-sort :refer :all]))
+
+
+(deftest quick-sort-test
+  (is (.equals (list 1 2 4 5) (quick-sort (list 2 4 1 5)))))
