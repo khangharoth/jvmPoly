209b686f49d169d5338b2d46af2d357d3300b773
Kuldeep Singh <kuldeepsinghk.com>
2019-08-15 21:05:50 +0530

    working QuickCheck in Clojure

--- clojure-core/src/test/clojure/bad_property.clj
+++ clojure-core/src/test/clojure/bad_property.clj
@@ -5,11 +5,14 @@
             [clojure.test.check.properties :as prop]
             ))
 
-(def bad-property
-  (prop/for-all [v (gen/vector gen/small-integer)]
+(defn bad-property [x]
+  (prop/for-all [v x]
+                (println v)
                 (or (empty? v) (apply <= v))))
 
-(println (:pass? (tc/quick-check 100 bad-property)))
+
 
 (deftest bad-property-test
-  (is (not (:pass? (tc/quick-check 100 bad-property)))))
+  (is (not (:pass? (tc/quick-check 10 (bad-property (gen/list gen/small-integer)))))))
+
+(bad-property-test)
