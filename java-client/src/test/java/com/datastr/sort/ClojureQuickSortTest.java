a30a0d1e88bbb72577170d70a313956d901805b2
Kuldeep Singh <kuldeepsinghk.com>
2019-08-18 18:40:15 +0530

    call to clojure quick sort from Java client

--- java-client/src/test/java/com/datastr/sort/ClojureQuickSortTest.java
+++ java-client/src/test/java/com/datastr/sort/ClojureQuickSortTest.java
@@ -2,24 +2,30 @@ package com.datastr.sort;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
+import clojure.lang.LazySeq;
 import org.jooq.lambda.Seq;
 import org.testng.annotations.Test;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 public class ClojureQuickSortTest {
     @Test
-    public void shouldCallClojureCode() {
+    public void shouldSortFourElementList() {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("data-str.quick-sort"));
 
-        IFn plus = Clojure.var("data-str.quick-sort", "quick-sort");
+        IFn quickSort = Clojure.var("data-str.quick-sort", "quick-sort");
 
 
         List<Integer> integers = Seq.of(4, 2, 3, 2).toList();
 
-        System.out.println(plus.invoke(integers));
+        LazySeq sortedSeq = (LazySeq) quickSort.invoke(integers);
+        assertEquals(sortedSeq.size(), integers.size());
+
+        assertTrue(sortedSeq.stream().sorted().collect(Collectors.toList()).equals(sortedSeq));
     }
 }
