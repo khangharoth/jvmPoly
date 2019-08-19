8b6c6901a551035c816a004cdcaeac407ea3ba10
Kuldeep Singh <kuldeepsinghk.com>
2019-08-19 20:33:46 +0530

    call to clojure quick sort from Java client

--- scala-core/src/test/scala/com/datastr/ScalaQuickSortSpec.scala
+++ scala-core/src/test/scala/com/datastr/ScalaQuickSortSpec.scala
@@ -1,28 +1,23 @@
 package com.datastr
 
-import java.util
-import java.util.function.Consumer
+import org.scalatest.{BeforeAndAfterAll, FunSuite, Matchers}
 
-import scala.collection.JavaConversions._
-import org.scalatest.FlatSpec
-import scala.collection.JavaConversions._
-
-class ScalaQuickSortSpec extends FlatSpec {
-  "List " should "have same no of elements After Sorting" in {
+class ScalaQuickSortSpec extends FunSuite with Matchers with BeforeAndAfterAll {
+  test("List  should have same no of elements After Sorting in") {
 
     val integers: List[Integer] = List[Integer](4, 2, 3, 2)
     val sortedList: List[Integer] = ScalaQuickSort.sort(integers)
     assert(sortedList.size == integers.size)
   }
 
-  "List elements" should "be in increasing order" in {
+
+  test("List elements should be in increasing order in") {
 
     val integers: List[Integer] = List[Integer](4, 2, 3, 2)
     val sortedList: List[Integer] = ScalaQuickSort.sort(integers)
 
-    sortedList.forEach(new Consumer[Integer] {
-      override def accept(t: Integer): Unit = println(t)
-    })
+
+    assert(integers.sorted == sortedList)
 
 
   }
