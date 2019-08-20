e9a0de8581fba7dca38ba8d106d0e4e61ec7a97e
Kuldeep Singh <kuldeepsinghk.com>
2019-08-21 05:27:29 +0530

    java selection sort going

--- java-client/src/main/java/com/datastr/sort/SelectionSort.java
+++ java-client/src/main/java/com/datastr/sort/SelectionSort.java
@@ -1,10 +1,23 @@
 package com.datastr.sort;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class SelectionSort implements SortAlgo {
     @Override
     public List<Integer> sort(List<Integer> elements) {
-        return elements;
+        Integer[] numbers = elements.toArray(new Integer[0]);
+
+        for (int i = 0; i < numbers.length; i++) {
+
+            for (int j = i; j < numbers.length; j++) {
+                if (numbers[i] > numbers[j]) {
+                    Integer tmp = numbers[i];
+                    numbers[i] = numbers[j];
+                    numbers[j] = tmp;
+                }
+            }
+        }
+        return Arrays.asList(numbers);
     }
 }
--- java-client/src/test/java/com/datastr/sort/SortingAlgoTest.java
+++ java-client/src/test/java/com/datastr/sort/SortingAlgoTest.java
@@ -4,13 +4,21 @@ import net.jqwik.api.*;
 import org.jooq.lambda.Seq;
 import org.testng.annotations.Test;
 
+
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.testng.Assert.assertEquals;
-import static org.testng.Assert.assertTrue;
 
-public class QuickSortTest {
+public class SortingAlgoTest {
+
+    @Test
+    public void shouldSortSimpleCase() {
+        List<Integer> randomList = Seq.of(3, 0, -1).toList();
+        List<Integer> sortedList = new SelectionSort().sort(randomList);
+        assertEquals(sortedList.size(), randomList.size());
+        assertEquals(sortedList, randomList.stream().sorted().collect(Collectors.toList()));
+    }
 
     @Property(shrinking = ShrinkingMode.FULL)
     public void shouldBeAbleToSortByQuickSort(@ForAll("integerLists") List<Integer> randomList) {
@@ -20,7 +28,6 @@ public class QuickSortTest {
     }
 
     @Property(shrinking = ShrinkingMode.FULL)
-    @Disabled
     public void shouldBeAbleToSortBySelectionSort(@ForAll("integerLists") List<Integer> randomList) {
         List<Integer> sortedList = new SelectionSort().sort(randomList);
         assertEquals(sortedList.size(), randomList.size());
