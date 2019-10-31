c6ea0108829af6aae1e297f878d1c0b8fc9c17b3
Kuldeep Singh <kuldeepsinghk.com>
2019-10-31 20:38:34 +0530

    High order function in Kotlin

--- java-client/src/test/java/com/datastr/highOrderFn/KotlinHighOrderFxnTest.java
+++ java-client/src/test/java/com/datastr/highOrderFn/KotlinHighOrderFxnTest.java
@@ -1,10 +1,11 @@
 package com.datastr.highOrderFn;
 
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 public class KotlinHighOrderFxnTest {
     @Test
-
+    @Ignore
     public void shouldCompose() {
 //        HighOrderFnKt.applyTwice(a-> a+3 ,10);
 
