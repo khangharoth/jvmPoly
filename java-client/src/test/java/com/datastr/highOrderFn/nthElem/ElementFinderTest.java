307637af21e3f1c1a51d69ef23e848696372fb95
Kuldeep Singh <kuldeepsinghk.com>
2019-11-04 21:11:38 +0530

    Seq for test cases

--- java-client/src/test/java/com/datastr/highOrderFn/nthElem/ElementFinderTest.java
+++ java-client/src/test/java/com/datastr/highOrderFn/nthElem/ElementFinderTest.java
@@ -1,50 +1,51 @@
 package com.datastr.highOrderFn.nthElem;
 
 import org.jooq.lambda.Seq;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 
 import java.util.Arrays;
 
+import static org.jooq.lambda.Seq.seq;
 import static org.testng.Assert.assertEquals;
 
 public class ElementFinderTest {
-    private final Iterable<String> letters = Arrays.asList("a", "b", "c", "d", "e");
-    private final Seq<String> lettersSeq = Seq.of("a", "b", "c", "d", "e");
     private FunctionCompositionFinder finder = new FunctionCompositionFinder();
     private MethodChainingFinder methodChainingFinder = new MethodChainingFinder();
 
-    @Test
-    @Ignore
-    public void shouldBeAbleToFindFirst() {
-        assertEquals(finder.first(lettersSeq), methodChainingFinder.first(lettersSeq));
+    private Seq<String> lettersSeq() {
+        return seq(Arrays.asList("a", "b", "c", "d", "e"));
+    }
 
-        assertEquals(methodChainingFinder.first(lettersSeq), "a");
-        assertEquals(finder.first(lettersSeq), "a");
+    @Test
+    public void shouldBeAbleToFindFirst() {
+        assertEquals(finder.first(lettersSeq()), methodChainingFinder.first(lettersSeq()));
+
+        assertEquals(methodChainingFinder.first(lettersSeq()), "a");
+        assertEquals(finder.first(lettersSeq()), "a");
     }
 
     @Test
     public void shouldBeAbleToFindSecond() {
-        assertEquals(finder.second(letters), methodChainingFinder.second(letters));
-        assertEquals(finder.second(letters), "b");
+        assertEquals(finder.second(lettersSeq()), methodChainingFinder.second(lettersSeq()));
+        assertEquals(finder.second(lettersSeq()), "b");
     }
 
     @Test
     public void shouldBeAbleToFindThird() {
-        assertEquals(finder.third(letters), methodChainingFinder.third(letters));
-        assertEquals(finder.third(letters), "c");
+        assertEquals(finder.third(lettersSeq()), methodChainingFinder.third(lettersSeq()));
+        assertEquals(finder.third(lettersSeq()), "c");
     }
 
     @Test
     public void shouldBeAbleToFindRest() {
-        assertEquals(finder.rest(letters), methodChainingFinder.rest(letters));
+        assertEquals(finder.rest(lettersSeq()), methodChainingFinder.rest(lettersSeq()));
     }
 
     @Test
     public void shouldBeAbleToFindNth() {
-        assertEquals(finder.<String>nth(0, letters), "a");
-        assertEquals(finder.<String>nth(3, letters), "d");
-        assertEquals(finder.<String>nth(4, letters), "e");
+        assertEquals(finder.<String>nth(0, lettersSeq()), "a");
+        assertEquals(finder.<String>nth(3, lettersSeq()), "d");
+        assertEquals(finder.<String>nth(4, lettersSeq()), "e");
     }
 
 }
