73aaf6f2819fe3cd2a4472bb58e19c4e97b8d14d
Kuldeep Singh <kuldeepsinghk.com>
2020-08-22 14:36:53 +0530

    pack bin tests in java

--- java-client/src/test/java/com/packBin/ClojurePackBinTest.java
+++ java-client/src/test/java/com/packBin/ClojurePackBinTest.java
@@ -2,7 +2,12 @@ package com.packBin;
 
 import clojure.java.api.Clojure;
 import clojure.lang.IFn;
+import clojure.lang.PersistentArrayMap;
+import clojure.lang.PersistentVector;
 import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
 
 public class ClojurePackBinTest {
     @Test
@@ -10,7 +15,23 @@ public class ClojurePackBinTest {
         IFn require = Clojure.var("clojure.core", "require");
         require.invoke(Clojure.read("com.pack-bin.pack-bin"));
 
-        IFn packBins = Clojure.var("com.pack-bin.pack-bin", "pack-bins");
 
+        IFn packbins = Clojure.var("com.pack-bin.pack-bin", "pack-bins");
+
+        PersistentVector bins = bins();
+        PersistentArrayMap containers = containers();
+
+        PersistentArrayMap result = (PersistentArrayMap) packbins.invoke(containers, bins);
+        assertEquals(result.size(), 3);
+    }
+
+    private PersistentArrayMap containers() {
+        IFn generatecontainers = Clojure.var("com.pack-bin.pack-bin", "generate-containers");
+        return (PersistentArrayMap) generatecontainers.invoke(3, 20);
+    }
+
+    private PersistentVector bins() {
+        IFn generatebins = Clojure.var("com.pack-bin.pack-bin", "generate-bins");
+        return (PersistentVector) generatebins.invoke(3, 20);
     }
 }
